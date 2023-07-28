688c4f89d46c5592a52f2d4e19e66c6e599ccedf
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-07-28 18:54:11 -0400

    sql lab done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex,color, temperament, alive) VALUES ("Mr.Chocolate" , 24, "M", "Brown" , "Furious", 1);
+INSERT INTO bears (name, age, sex,color, temperament, alive) VALUES ("Rowdy" , 24, "M", "Black" , "Furious", 1);
+INSERT INTO bears (name, age, sex,color, temperament, alive) VALUES ("Tabitha" , 24, "F", "white" , "Furious", 1);
+INSERT INTO bears (name, age, sex,color, temperament, alive) VALUES ("Sergeant Brown" , 24, "M", "Brown" , "Furious", 1);
+INSERT INTO bears (name, age, sex,color, temperament, alive) VALUES ("Melissa" , 24, "F", "white" , "Furious", 1);
+INSERT INTO bears (name, age, sex,color, temperament, alive) VALUES ("Grinch" , 24, "M", "green" , "Furious", 1);
+INSERT INTO bears (name, age, sex,color, temperament, alive) VALUES ("Wendy" , 34, "M", "White" , "Furious", 1);
+INSERT INTO bears (name, age, sex,color, temperament, alive) VALUES (NULL , NULL, NULL, NULL , NULL, 0);
